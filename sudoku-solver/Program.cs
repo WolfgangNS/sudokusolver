1b99a8a64bb8876f04623e60a14e67e773cb6f32
WolfgangNS <WolfgangStark7@gmail.com>
2020-09-10 18:06:47 -0400

    Update Program.cs
    Updated so that it animates properly, and modifies tiles at a position instead of reprinting the entire board.

--- sudoku-solver/Program.cs
+++ sudoku-solver/Program.cs
@@ -32,6 +32,40 @@ namespace sudoku_solver
             0b_0000_0000_0000, //obligatory 0
             0b_0000_0000_0001,
             0b_0000_0000_0010,
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace sudoku_solver
+{
+    class Program
+    {
+        // static int[,] board = {
+        // {5,3,0,0,7,0,0,0,0},
+        // {6,0,0,1,9,5,0,0,0},
+        // {0,9,8,0,0,0,0,6,0},
+        // {8,0,0,0,6,0,0,0,3},
+        // {4,2,6,8,5,3,7,9,1}, //changed to be fully solved
+        // {7,0,0,0,2,0,0,0,6},
+        // {0,6,0,0,0,0,2,8,0},
+        // {0,0,0,4,1,9,0,0,5},
+        // {0,0,0,0,8,0,0,7,9},
+        // };
+        static int[,] board = {
+        {0,3,0,0,1,0,0,6,0},
+        {7,5,0,0,3,0,0,4,8},
+        {0,0,6,9,8,4,3,0,0},
+        {0,0,3,0,0,0,8,0,0},
+        {9,1,2,0,0,0,6,7,4},
+        {0,0,4,0,0,0,5,0,0},
+        {0,0,1,6,7,5,2,0,0},
+        {6,8,0,0,9,0,0,1,5},
+        {0,9,0,0,4,0,0,3,0},
+        };
+        public static Int32[] val = {
+            0b_0000_0000_0000, //obligatory 0
+            0b_0000_0000_0001,
+            0b_0000_0000_0010,
             0b_0000_0000_0100,
             0b_0000_0000_1000,
             0b_0000_0001_0000,
@@ -42,7 +76,13 @@ namespace sudoku_solver
         };
         static void Main(string[] args)
         {
+            Console.WindowWidth = 20;
+            Console.WindowHeight = 14;
+
             print();
+            Console.ReadKey();
+            ///Thread.Sleep(3000);
+
             //1. initiate truth tables
             var checks = new Int32[9, 9];
             for (int i = 0; i < 9; i++)
@@ -105,6 +145,77 @@ namespace sudoku_solver
                         }
                         //Console.WriteLine(Convert.ToString(flags, 2));
                         //Console.ReadLine();
+
+                        for (int row = 0; row < 3; row++)
+                        {
+                            for (int col = 0; col < 3; col++)
+                            {
+                                checks[boxrow * 3 + row, boxcol * 3 + col] &= ~flags;
+                            }
+                        }
+                    }
+                }
+
+                //5. attempt solve on each item, row by row, col by col
+                for (int row = 0; row < 9; row++)
+                {
+                    for (int col = 0; col < 9; col++)
+                    {
+                        if (board[row, col] == 0)
+                        {
+                            var final = Array.IndexOf(val, checks[row, col]); //these ifs can be combined?
+                            if (final != -1)
+                            {
+                                board[row, col] = final;
+                                printxy(col, row, final);
+                                //print();
+                            }
+                        }
+                    }
+                }
+                ab++;
+            }
+            Console.ReadKey();
+        }
+        public static void print()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (board[i, j] != 0)
+                    {
+                        Console.Write(" " + board[i, j]);
+                        
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine("-------------------");
+            Thread.Sleep(200);
+        }
+        public static void printxy(int x, int y, int val)
+        {
+            Console.SetCursorPosition(x * 2 + 1, y);
+            if (board[y, x] != 0) //row, col?
+            {
+                Console.Write(val);
+            }
+            else
+            {
+                Console.Write(" ");
+            }
+            Thread.Sleep(200);
+        }
+    }
+}
+//TO-DO: implement strategies from here: https://www.learn-sudoku.com/advanced-techniques.html
+//TO-DO: allow import/export
+//TO-DO: open new console window, update sudoku board at the given positions
 
                         for (int row = 0; row < 3; row++)
                         {
