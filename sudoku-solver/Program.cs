1c8687bdaf0ff5ef471dd27da609f3d06db2c5e8
WolfgangNS <WolfgangStark7@gmail.com>
2020-09-10 18:13:42 -0400

    Update Program.cs
    (fixed reverted commit) Updated so that it animates properly, and modifies tiles at a position instead of reprinting the entire board.

--- sudoku-solver/Program.cs
+++ sudoku-solver/Program.cs
@@ -1,37 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-
-namespace sudoku_solver
-{
-    class Program
-    {
-        // static int[,] board = {
-        // {5,3,0,0,7,0,0,0,0},
-        // {6,0,0,1,9,5,0,0,0},
-        // {0,9,8,0,0,0,0,6,0},
-        // {8,0,0,0,6,0,0,0,3},
-        // {4,2,6,8,5,3,7,9,1}, //changed to be fully solved
-        // {7,0,0,0,2,0,0,0,6},
-        // {0,6,0,0,0,0,2,8,0},
-        // {0,0,0,4,1,9,0,0,5},
-        // {0,0,0,0,8,0,0,7,9},
-        // };
-        static int[,] board = {
-        {0,3,0,0,1,0,0,6,0},
-        {7,5,0,0,3,0,0,4,8},
-        {0,0,6,9,8,4,3,0,0},
-        {0,0,3,0,0,0,8,0,0},
-        {9,1,2,0,0,0,6,7,4},
-        {0,0,4,0,0,0,5,0,0},
-        {0,0,1,6,7,5,2,0,0},
-        {6,8,0,0,9,0,0,1,5},
-        {0,9,0,0,4,0,0,3,0},
-        };
-        public static Int32[] val = {
-            0b_0000_0000_0000, //obligatory 0
-            0b_0000_0000_0001,
-            0b_0000_0000_0010,
 using System;
 using System.Collections.Generic;
 using System.Threading;
@@ -210,62 +176,6 @@ namespace sudoku_solver
                 Console.Write(" ");
             }
             Thread.Sleep(200);
-        }
-    }
-}
-//TO-DO: implement strategies from here: https://www.learn-sudoku.com/advanced-techniques.html
-//TO-DO: allow import/export
-//TO-DO: open new console window, update sudoku board at the given positions
-
-                        for (int row = 0; row < 3; row++)
-                        {
-                            for (int col = 0; col < 3; col++)
-                            {
-                                checks[boxrow * 3 + row, boxcol * 3 + col] &= ~flags;
-                            }
-                        }
-                    }
-                }
-
-                //5. attempt solve on each item, row by row, col by col
-                for (int row = 0; row < 9; row++)
-                {
-                    for (int col = 0; col < 9; col++)
-                    {
-                        if (board[row, col] == 0)
-                        {
-                            var final = Array.IndexOf(val, checks[row, col]);
-                            if (final != -1)
-                            {
-                                board[row, col] = final;
-                                print();
-                            }
-                        }
-                    }
-                }
-                ab++;
-            }
-            Console.WriteLine("Done.");
-        }
-        public static void print()
-        {
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    if (board[i, j] != 0)
-                    {
-                        Console.Write(" " + board[i, j].ToString());
-                    }
-                    else
-                    {
-                        Console.Write("  ");
-                    }
-                }
-                Console.Write("\n");
-            }
-            Console.WriteLine("-------------------");
-            System.Threading.Thread.Sleep(200);
         }
     }
 }
