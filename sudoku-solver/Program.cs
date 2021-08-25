c886e2bd01761c4a15486f1e07302a33d5fe5eba
WolfgangNS <WolfgangStark7@gmail.com>
2021-08-25 03:31:51 -0400

    Included csvwriter for diagnostic purposes

--- sudoku-solver/Program.cs
+++ sudoku-solver/Program.cs
@@ -1,23 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Threading;
+using CsvHelper;
+using System.IO;
 
 namespace sudoku_solver
 {
     class Program
     {
-        // static int[,] board = {
-        // {5,3,0,0,7,0,0,0,0},
-        // {6,0,0,1,9,5,0,0,0},
-        // {0,9,8,0,0,0,0,6,0},
-        // {8,0,0,0,6,0,0,0,3},
-        // {4,2,6,8,5,3,7,9,1}, //changed to be fully solved
-        // {7,0,0,0,2,0,0,0,6},
-        // {0,6,0,0,0,0,2,8,0},
-        // {0,0,0,4,1,9,0,0,5},
-        // {0,0,0,0,8,0,0,7,9},
-        // };
-        static int[,] board = {
+        static int[,] board1 = {
         {0,3,0,0,1,0,0,6,0},
         {7,5,0,0,3,0,0,4,8},
         {0,0,6,9,8,4,3,0,0},
@@ -28,6 +19,17 @@ namespace sudoku_solver
         {6,8,0,0,9,0,0,1,5},
         {0,9,0,0,4,0,0,3,0},
         };
+        static int[,] board2 = {
+        {0,0,0,7,9,0,0,5,0},
+        {3,5,2,0,0,8,0,4,0},
+        {0,0,0,0,0,0,0,8,0},
+        {0,1,0,0,7,0,0,0,4},
+        {6,0,0,3,0,1,0,0,8},
+        {9,0,0,0,8,0,0,1,0},
+        {0,2,0,0,0,0,0,0,0},
+        {0,4,0,5,0,0,8,9,1},
+        {0,8,0,0,3,7,0,0,0}
+        }; //harder difficulty, not able to solve yet
         public static Int32[] val = {
             0b_0000_0000_0000, //obligatory 0
             0b_0000_0000_0001,
@@ -42,10 +44,11 @@ namespace sudoku_solver
         };
         static void Main(string[] args)
         {
-            Console.WindowWidth = 20;
-            Console.WindowHeight = 14;
+            var board = board2;
+            //Console.WindowWidth = 20;
+            //Console.WindowHeight = 14;
 
-            print();
+            print(board);
             Console.ReadKey();
             ///Thread.Sleep(3000);
 
@@ -133,7 +136,7 @@ namespace sudoku_solver
                             if (final != -1)
                             {
                                 board[row, col] = final;
-                                printxy(col, row, final);
+                                printxy(ref board, col, row, final);
                                 //print();
                             }
                         }
@@ -141,10 +144,40 @@ namespace sudoku_solver
                 }
                 ab++;
             }
-            Console.ReadKey();
+            //Console.ReadKey();
+            Console.SetCursorPosition(0, 11);
+
+
+            //write into file for debugging, manual solving:
+            var filestream = File.Create("puzzle.csv");
+            var sw = new StreamWriter(filestream);
+            var csvwriter = new CsvWriter(sw, System.Globalization.CultureInfo.CurrentCulture);
+            for(int i = 1;i<=9;i++)
+            {
+            csvwriter.WriteField("col"+i);
+            }
+            csvwriter.NextRecord();
+            for(int row = 1;row<=9;row++)
+            {
+                for(int col=1;col<=9;col++)
+                {
+                    if(board[row-1,col-1]!=0)
+                    {
+                        csvwriter.WriteField(board[row-1,col-1]);
+                    }else
+                    {
+                        csvwriter.WriteField(flagtostring(checks[row-1,col-1]));
+                    }
+                }
+                csvwriter.NextRecord();
+            }
+            sw.Close();
+            //sw.WriteLine("cool!");
+
         }
-        public static void print()
+        public static void print(int[,] board)
         {
+            Console.Clear();
             for (int i = 0; i < 9; i++)
             {
                 for (int j = 0; j < 9; j++)
@@ -164,7 +197,7 @@ namespace sudoku_solver
             Console.WriteLine("-------------------");
             Thread.Sleep(200);
         }
-        public static void printxy(int x, int y, int val)
+        public static void printxy(ref int[,] board, int x, int y, int val)
         {
             Console.SetCursorPosition(x * 2 + 1, y);
             if (board[y, x] != 0) //row, col?
@@ -177,8 +210,24 @@ namespace sudoku_solver
             }
             Thread.Sleep(200);
         }
+        public static string flagtostring(Int32 input)
+        {
+            var buffer = "";
+            for(int i = 1;i<=11;i++) //for some reason it wasn't going to 9
+            {
+                if((input&(0x1<<(i-1)))!=0)
+                {
+                    buffer+=i.ToString()+" ";
+                }
+            }
+            return buffer;
+        }
     }
 }
 //TO-DO: implement strategies from here: https://www.learn-sudoku.com/advanced-techniques.html
 //TO-DO: allow import/export
 //TO-DO: open new console window, update sudoku board at the given positions
+
+//in a given row, if there are three boxes with the same three options, 
+//those three can be elimated from the rest (same with any number)
+
